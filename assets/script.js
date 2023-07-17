162d9077768ee32ee471826d2db4fd74c25c8d77
NickUCLA <133281012+NickUCLA@users.noreply.github.com>
2023-07-17 13:33:57 -0700

    added starter code

---
+++ assets/script.js
@@ -0,0 +1,3 @@
+let api = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}`;
+
+let cities = `http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={5}&appid={API key}`;
